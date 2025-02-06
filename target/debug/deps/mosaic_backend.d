e9a1a613e964c8b6aa48bdaa801cfcf6ad9735e7
Anthony Green <anthony.j.green@outlook.com>
2025-02-06 09:11:07 -0800

    hello world!

---
+++ target/debug/deps/mosaic_backend.d
@@ -0,0 +1,5 @@
+C:\Users\antho\Documents\Code\mosaic\mosaic-backend\target\debug\deps\mosaic_backend.exe: src/main.rs
+
+C:\Users\antho\Documents\Code\mosaic\mosaic-backend\target\debug\deps\mosaic_backend.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/mosaic-backend.d
@@ -0,0 +1,1 @@
+C:\Users\antho\Documents\Code\mosaic\mosaic-backend\target\debug\mosaic-backend.exe: C:\Users\antho\Documents\Code\mosaic\mosaic-backend\src\main.rs
