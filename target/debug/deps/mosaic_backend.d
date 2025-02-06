30de1f939a2d861aab0119673b96309d22a54290
Anthony Green <anthony.j.green@outlook.com>
2025-02-06 09:54:48 -0800

    removed target

--- target/debug/deps/mosaic_backend.d
+++
@@ -1,5 +0,0 @@
-C:\Users\antho\Documents\Code\mosaic\mosaic-backend\target\debug\deps\mosaic_backend.exe: src/main.rs
-
-C:\Users\antho\Documents\Code\mosaic\mosaic-backend\target\debug\deps\mosaic_backend.d: src/main.rs
-
-src/main.rs:
--- target/debug/mosaic-backend.d
+++
@@ -1,1 +0,0 @@
-C:\Users\antho\Documents\Code\mosaic\mosaic-backend\target\debug\mosaic-backend.exe: C:\Users\antho\Documents\Code\mosaic\mosaic-backend\src\main.rs
